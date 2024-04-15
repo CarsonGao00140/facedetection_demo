deabb8b9d6a012f73079677ec00088361f939eac
CarsonGao00140 <156142273+CarsonGao00140@users.noreply.github.com>
2024-04-15 01:32:14 -0400

    Update

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
-let img = document.getElementById("image");
-let overLayer = document.getElementById("overlay");
-let dialog = document.getElementById("dialog");
-let alert = document.getElementById("alert");
-let buttonOption1 = document.getElementById("option1");
-let buttonOption2 = document.getElementById("option2");
+let buttons = document.querySelectorAll("button");
+let alert = document.querySelector("p");
+let img = document.querySelector("img");
+let overLayer = document.querySelector("div");
 
-dialog.showModal();
-
-buttonOption1.onclick = () => {
+buttons[0].onclick = () => {
     (async () => {
         let clipboard = await navigator.clipboard.readText()
         let response = await fetch(clipboard);
@@ -17,42 +13,43 @@ buttonOption1.onclick = () => {
     detect();
 }
 
-buttonOption2.onclick = () => {
+buttons[1].onclick = () => {
     img.src = "./img/Carson.png";
     detect();
 }
 
 function detect() {
     img.onload = () => {
-        overLayer.viewBox.baseVal.width = img.naturalWidth;
-        overLayer.viewBox.baseVal.height = img.naturalHeight;
-        let content = new DocumentFragment;
         let faces = media.detect(img).detections;
-        for (face of faces) {
-            let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-            rect.setAttribute("x", face.boundingBox.originX);
-            rect.setAttribute("y", face.boundingBox.originY);
-            rect.setAttribute("width", face.boundingBox.width);
-            rect.setAttribute("height", face.boundingBox.height);
-            content.append(rect);
-            for (point of face.keypoints) {
-                let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                circle.setAttribute("cx", point.x * img.naturalWidth);
-                circle.setAttribute("cy", point.y * img.naturalHeight);
-                circle.setAttribute("r", "1");
-                content.appendChild(circle);
-            }
-            overLayer.appendChild(content);
-            let text = document.createElement("p")
-            text.textContent = (face.categories[0].score * 100).toFixed(1) + "%";
-            text.style.top = (face.boundingBox.originY + face.boundingBox.height) / img.naturalHeight * 100 + "%";
-            text.style.left = face.boundingBox.originX/ img.naturalWidth * 100 + "%";
-            text.classList.add("confidence");
-            overLayer.after(text);
-        }
+        let content = new DocumentFragment;
         if (!faces.length) {
             alert.textContent = "No face detected!";
-            dialog.showModal();
+        } else {
+            alert.textContent = "";
+            let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            svg.setAttribute("viewBox", `0 0 ${img.naturalWidth} ${img.naturalHeight}`);
+            for (face of faces) {
+                let rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+                rect.setAttribute("x", face.boundingBox.originX);
+                rect.setAttribute("y", face.boundingBox.originY);
+                rect.setAttribute("width", face.boundingBox.width);
+                rect.setAttribute("height", face.boundingBox.height);
+                svg.appendChild(rect);
+                for (point of face.keypoints) {
+                    let circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+                    circle.setAttribute("cx", point.x * img.naturalWidth);
+                    circle.setAttribute("cy", point.y * img.naturalHeight);
+                    circle.setAttribute("r", "1");
+                    svg.appendChild(circle);
+                }
+                let text = document.createElement("p")
+                text.textContent = (face.categories[0].score * 100).toFixed(1) + "%";
+                text.style.top = (face.boundingBox.originY + face.boundingBox.height) / img.naturalHeight * 100 + "%";
+                text.style.left = face.boundingBox.originX/ img.naturalWidth * 100 + "%";
+                text.classList.add("confidence");
+                content.append(svg, text);
+            }
         }
+        overLayer.replaceChildren(content);
     }
 }
